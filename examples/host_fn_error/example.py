86faf9cf435241c49e480b3e301f5c5b9b0aebbc
Sander Bos <sander@streamhpc.com>
2026-03-23 21:22:31 +0100

    tests: add examples/host_fn_error

---
+++ examples/host_fn_error/example.py
@@ -0,0 +1,22 @@
+import time
+
+import grug
+from grug import GrugState
+from grug.entity import GameFnError
+
+state = grug.init()
+
+
+@state.game_fn
+def print_string(state: GrugState, string: str):
+    if string == "":
+        raise GameFnError("print_string() received an empty string")
+    print(string)
+
+
+file = state.compile_grug_file("animals/labrador-Dog.grug")
+dog1 = file.create_entity()
+
+state.update()
+dog1.on_bark("woof")
+dog1.on_bark("")
